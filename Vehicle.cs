07997f9229eb4048487da9a612c5d3bf3ed2de92
peteromneus <271960984+peteromneus@users.noreply.github.com>
2026-09-16 17:29:51 +0200

    Demonstrate encapsulation

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,10 +1,21 @@
 ﻿public class Vehicle
 {
-    public string Brand { get; set; }
+    public string Brand { get; private set; } = "Unknown";
 
     public Vehicle(string brand)
     {
-        Brand = brand;
+        ChangeBrand(brand);
+    }
+
+    public bool ChangeBrand(string newBrand)
+    {
+        if (string.IsNullOrWhiteSpace(newBrand))
+        {
+            return false;
+        }
+
+        Brand = newBrand;
+        return true;
     }
 
     public void PrintInfo()
